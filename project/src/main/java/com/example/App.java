cd1be83df2b52ce5b389bcbc3e49f24fbcd1c17a
matthewmorena <73557066+matthewmorena@users.noreply.github.com>
2024-05-01 11:00:50 -0400

    Update App.java

--- project/src/main/java/com/example/App.java
+++ project/src/main/java/com/example/App.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.io.*;
+import java.util.*;
 import org.apache.spark.sql.*;
 import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.regression.LinearRegression;
@@ -17,31 +18,34 @@ public class App {
             .config("spark.master", "local")
             .getOrCreate();
 
-        Dataset<Row> trainingData = spark.read()
+            Dataset<Row> trainingData = spark.read()
             .format("csv")
             .option("inferSchema", "true")
             .option("header", "true")
             .load("TrainingDataset.csv");
 
         VectorAssembler assembler = new VectorAssembler()
-            .setInputCols(new String[]{"fixed acidity", "volatile acidity", "citric acid", "residual sugar", 
+            .setInputCols(new String[]{"fixed acidity", "volatile acidity", "citric acid", "residual sugar",
                 "chlorides", "free sulfur dioxide", "total sulfur dioxide", "density", "pH", "sulphates", "alcohol"})
             .setOutputCol("features");
 
         Dataset<Row> output = assembler.transform(trainingData);
 
-        LinearRegression lr = new LinearRegression()
-            .setFeaturesCol("features")
-            .setLabelCol("quality");
-        
-        LinearRegressionModel model = lr.fit(output);
+        try {
+                LinearRegression lr = new LinearRegression()
+                        .setFeaturesCol("features")
+                        .setLabelCol("quality");
 
-        model.write().overwrite().save("project/model");
+                LinearRegressionModel model = lr.fit(output);
 
-        Pipeline pipeline = new Pipeline().setStages(new PipelineStage[]{assembler, lr});
-        PipelineModel pipelineModel = pipeline.fit(output);
-        pipelineModel.write().overwrite().save("project/pipeline");
+                model.write().overwrite().save("project/model");
 
+                Pipeline pipeline = new Pipeline().setStages(new PipelineStage[]{assembler, lr});
+                PipelineModel pipelineModel = pipeline.fit(output);
+                pipelineModel.write().overwrite().save("project/pipeline");
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
         spark.stop();
     }
 }
